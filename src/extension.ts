28e1c3d3d419b424adb3a422638fda4ec114c5a4
Nevil <nevilclavain@hotmail.fr>
2022-03-05 11:14:07 +0100

    added sources

---
+++ src/extension.ts
@@ -0,0 +1,23 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+	console.log('kfront extension activated');
+	context.subscriptions.push(vscode.commands.registerCommand('kfront.refresh', refresh));
+
+	refresh();
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+	console.log('kfront extension Deactivated');
+}
+
+function refresh() {
+	vscode.window.showInformationMessage('kfront DISPLAY Refresh');
+	console.log('kfront DISPLAY Refresh');
+}
