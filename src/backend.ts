c72196af70830c0c13a5da79d5534635783f6f4b
Nevil <nevilclavain@hotmail.fr>
2022-03-26 12:08:02 +0100

    GET implemented in backend.ts

--- src/backend.ts
+++ src/backend.ts
@@ -1,23 +1,54 @@
 import * as vscode from 'vscode';
+import * as http from 'http';
+import { RequestOptions } from 'https';
 
 export function getStatus(callback: StatusSink) {
 
     console.log('getStatus() IN');
-    let backendURL = vscode.workspace.getConfiguration('khub').get('backendUrl');
+    let backendURL = vscode.workspace.getConfiguration('khub').get<string>('backendUrl');
 
-    var options = {
+    let opts: RequestOptions = {
         'method': 'GET',        
-        'hostname': backendURL,
+        'host': backendURL,
+        //'host': 'www.google.fr',
         'path': '/status',
         'headers': {
-        },
-        'maxRedirects': 20
+        }
     };
 
-    console.log('options hostname = ' + options.hostname);
+    console.log('options hostname = ' + opts.host);
 
-    let statusResult = new Object();
+    /*
+    let statusResult = new Object();    
     callback.display(statusResult);
+    */
+
+    var req = http.request(opts, function (res) {
+        
+        let chunks = '';    
+        res.on("data", function (chunk) {            
+            chunks += chunk;
+        });
+        res.on("end", function () {
+
+            console.log('server response is ' + chunks);
+
+        });        
+        res.on("error", function (error) {  
+
+            console.log('unexpected error: ' + res); 
+            vscode.window.showErrorMessage('kfront:' + res);     
+        });
+        
+    });
+    
+    req.on('error', function(res) { 
+        console.log('unexpected error: ' + res); 
+        vscode.window.showErrorMessage('kfront:' + res); 
+    });
+
+    req.end(); 
+
     
     console.log('getStatus() OUT');
 }
